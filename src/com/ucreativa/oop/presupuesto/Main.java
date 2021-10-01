ecc7f89f3f71bd39339812590fd946d9aeeb7055
marco.vargas <mvargas@accuray.com>
2021-09-30 20:46:08 -0600

    Cuarto Trabajo en Clase

--- src/com/ucreativa/oop/presupuesto/Main.java
+++ src/com/ucreativa/oop/presupuesto/Main.java
@@ -1,32 +1,50 @@
 package com.ucreativa.oop.presupuesto;
 
-import java.util.ArrayList;
+import com.ucreativa.oop.presupuesto.entidades.Gasto;
+import com.ucreativa.oop.presupuesto.entidades.RegistroGastos;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Mi nuevo Main");
-
-        Gasto gastoLuz = new Gasto();
-        gastoLuz.nombre = "Luz";
-        gastoLuz.moneda = "Colones";
-        gastoLuz.monto = 22000;
-
-        Gasto gastoInternet = new Gasto();
-        gastoInternet.nombre = "Internet";
-        gastoInternet.moneda = "Colones";
-        gastoInternet.monto = 30000;
-
+        System.out.println("Sistema Registro de Gastos");
+        Scanner consola = new Scanner(System.in);
+        boolean siga = true;
         RegistroGastos registo = new RegistroGastos();
-        registo.gastos = new ArrayList<>();
-        registo.gastos.add(gastoLuz);
-        registo.gastos.add(gastoInternet);
 
+        while (siga) {
+            System.out.println("Digite el nombre de su gasto:");
+            String nombre = consola.nextLine();
 
-        for (int i = 0; i < registo.gastos.size(); i++) {
-            System.out.println(registo.gastos.get(i).nombre);
+            System.out.println("Digite la Moneda:");
+            String moneda = consola.nextLine();
+
+            System.out.println("Digite la categoria de su gasto:");
+            String categoria = consola.nextLine();
+
+            System.out.println("Digite el monto de su gasto:");
+            String montoStr = consola.nextLine();
+            int monto = Integer.parseInt(montoStr);
+
+            Gasto nuevoGasto = new Gasto(nombre,
+                    moneda,
+                    categoria,
+                    monto);
+
+            registo.addGastos(nuevoGasto);
+
+            for (Gasto gastico : registo.getGastos()) {
+                System.out.println(gastico.getNombre());
+            }
+
+            System.out.println("Quiere Seguir?(`S`)");
+            siga = consola.nextLine().equals("S");
         }
+
+
+
     }
 
 }
