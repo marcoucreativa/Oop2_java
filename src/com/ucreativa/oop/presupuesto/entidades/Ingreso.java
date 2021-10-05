7add60be656c87ab4d8f2c0d2d89075af1a13ebb
marco.vargas <mvargas@accuray.com>
2021-10-04 20:46:12 -0600

    Quinto Trabajo en Clase

---
+++ src/com/ucreativa/oop/presupuesto/entidades/Ingreso.java
@@ -0,0 +1,20 @@
+package com.ucreativa.oop.presupuesto.entidades;
+
+public class Ingreso extends Movimiento{
+
+    private String periodicidad;
+
+    public Ingreso(String nombre,
+                   String moneda,
+                   String categoria,
+                   int monto,
+                   String periodicidad){
+        super(nombre, moneda, categoria, monto);
+        this.periodicidad = periodicidad;
+    }
+
+    @Override
+    public String getNombre(){
+        return super.getNombre() + " " + this.periodicidad;
+    }
+}
